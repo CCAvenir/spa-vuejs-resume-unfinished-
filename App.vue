dfbd92db8b4979647204ca1b78ddd42d6c120e5d
CCAvenir <ccavenir@student.hau.edu.ph>
2024-09-15 23:56:29 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,83 @@
+<template>
+  <main class="container">
+    <div id="resume" class="d-flex">
+      <div class="left-col">
+        <h4 class="section-headline">
+          About me
+        </h4>
+      <div>
+        I am a passionate Frontend Developer with over 3 years of experience in building responsive and dynamic web applications. 
+        My expertise lies in creating clean, user-friendly interfaces using Vue.js, Bootstrap, and JavaScript, along with a solid understanding of full-stack development principles.
+      </div>
+        <h4 class="section-headline">
+          Contact
+        </h4>
+        <ul>
+          <li>09669523502</li>
+          <li>avenirchristian22@gmail.com</li>
+          <li>San Fernando City, Pampanga</li>
+        </ul>
+        <h4 class="section-headline">
+          Skills
+        </h4>
+        <ul>
+          <li>Vue.js</li>
+          <li>JavaScript</li>
+          <li>HTML and CSS</li>
+          <li>Bootstrap</li>
+          <li>Node.js</li>
+          <li>RESTful API</li>
+          <li>GIT and Github</li>
+        </ul>
+        <h4 class="section-headline">
+          Certification
+        </h4>
+        <ul>
+          <li>Full-Stack Web Development Certification</li>
+        </ul>
+      </div>
+      <div class="right-col">
+        <h4 class="section-headline">Avenir, Christian C.</h4>
+        <h4 class="section-headline">Frontend Developer</h4>
+        <h4 class="section-headline">
+          Experience
+        </h4>
+        <div>company: Web Solutions Co.</div>
+        <div> role: Junior Web Developer</div>
+        <div>Date: "May 20xx to january 20xx"</div>
+        <ul>
+          <li>Built and maintained websites using HTML, CSS, JavaScript, and Bootstrap.</li>
+          <li>Assisted in creating interactive web components using Vue.js.</li>
+          <li>"Worked with senior developers on various full-stack web projects.</li>
+          <li>"Managed version control using Git and GitHub.</li>
+        </ul>
+        <h4 class="section-headline">
+          Education
+        </h4>
+        <div> school: Holy Angel Univercity</div>
+        <div> degree: Full-Stack Web Development Certification</div>
+        <div>Date: 2020-2025</div>
+        <ul>
+          <li>Did a lot of Web development courses.</li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  #resume {
+    box-shadow: (50, 50, 93, 0.25) 0px 13px 27px -5px, (0, 0, 0, 0.3) 0px 8px 16px, -8px;
+    height: 297mm;
+    width: 210mm;
+  }
+
+  .left-col {
+    width: 30%;
+  }
+
+  .right-col {
+    width: 70%;
+  }
+  
+</style>
